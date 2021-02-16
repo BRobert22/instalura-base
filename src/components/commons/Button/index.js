2b9e213644bc7ed0d9dc89117d6e306ba2250c3b
Robert B. Guimarães <robertb.guimaraes@hotmail.com>
2021-02-15 21:49:13 -0300

    Inclusão de breakpoint de midia para mudanças de layout com base na resolução da tela

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariantsMap } from '../foundation/Text'
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
 const ButtonGhost = css`
     color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
@@ -19,6 +20,17 @@ export const Button = styled.button`
     font-weight: bold;
     opacity: 1;
     border-radius: 8px;
+
+    ${breakpointsMedia({
+    xs: css`
+      ${TextStyleVariantsMap.smallestException}
+    `,
+    md: css`
+      padding: 12px 43px;
+      ${TextStyleVariantsMap.paragraph1}
+    `,
+})}
+
     ${TextStyleVariantsMap.smallestException}
     ${function (props) {
         return props.ghost ? ButtonGhost : ButtonDefault;
